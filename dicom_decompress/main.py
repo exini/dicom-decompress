5f2faadb45321068a5cccbd152a1220542a2757d
karl.sjostrand@exini.com <karl.sjostrand@exini.com>
2021-03-09 11:25:46 -0500

    Added draft support for palette images

--- dicom_decompress/main.py
+++ dicom_decompress/main.py
@@ -22,26 +22,41 @@ ts_decompress = [
     '1.2.840.10008.1.2.4.93',
 ]
 
+pi_rgb = 'RGB'
+pi_palette = 'PALETTE COLOR'
+
 supported_photometric_interpretations = [
     'YBR_FULL',
     'YBR_FULL_422',
+    pi_palette,
 ]
-
-rgb_photometric_interpretation = 'RGB'
 
 
 def decompress(dataset, ts):
     try:
-        dataset.decompress('pylibjpeg')
-    except Exception as e:
-        sys.stderr.write(f'Decompression of transfer syntax {ts} failed. Reason: {e}\n')
+        dataset.decompress()
+    except:
+        try:
+            dataset.decompress('pylibjpeg')
+        except Exception as e:
+            sys.stderr.write(f'Decompression of transfer syntax {ts} failed. Reason: {e}\n')
 
 
 def transcode(dataset, pi):
     try:
-        rgb = pydicom.pixel_data_handlers.convert_color_space(dataset.pixel_array, pi, rgb_photometric_interpretation)
+        arr = dataset.pixel_array
+        if pi == pi_palette:
+            rgb = pydicom.pixel_data_handlers.util.apply_color_lut(arr, dataset)
+            dataset.SamplesPerPixel = 3
+            dataset.PlanarConfiguration = 0
+        else:
+            rgb = pydicom.pixel_data_handlers.convert_color_space(arr, pi, pi_rgb)
+        rgb = (rgb / 256).astype('uint8') if rgb.dtype == 'uint16' else rgb
         dataset.PixelData = rgb.tobytes()
-        dataset.PhotometricInterpretation = rgb_photometric_interpretation
+        dataset.PhotometricInterpretation = pi_rgb
+        dataset.BitsAllocated = 8
+        dataset.BitsStored = 8
+        dataset.HighBit = 7
     except Exception as e:
         sys.stderr.write(f'Transcoding from {pi} to RGB failed. Reason: {e}\n')
 
@@ -57,26 +72,26 @@ def main():
     try:
         dataset = pydicom.dcmread(in_file, force=True)
 
+        if 'TransferSyntaxUID' not in dataset.file_meta:
+            dataset.file_meta.TransferSyntaxUID = pydicom.uid.ImplicitVRLittleEndian
+
+        if 'PhotometricInterpretation' not in dataset:
+            dataset.PhotometricInterpretation = 'MONOCHROME2'
+
         ts = dataset.file_meta.TransferSyntaxUID
         pi = dataset.PhotometricInterpretation
 
-        if ts is not None:
-            if ts in ts_decompress:
-                decompress(dataset, ts)
-                sys.stdout.write(f"Decompression successful from transfer syntax UID {ts}\n")
-            elif ts in ts_skip:
-                sys.stdout.write(f"Transfer syntax UID {ts} is uncompressed, skipping\n")
-            else:
-                sys.stdout.write(f"Transfer syntax UID {ts} not supported, skipping\n")
-
-            if pi is not None:
-                if pi in supported_photometric_interpretations:
-                    transcode(dataset, pi)
-                    sys.stdout.write(f"Transcoding successful from photometric interpretation {pi}\n")
-            else:
-                sys.stderr.write(f"Photometric interpretation missing\n")
+        if ts in ts_decompress:
+            decompress(dataset, ts)
+            sys.stdout.write(f"Decompression successful from transfer syntax UID {ts}\n")
+        elif ts in ts_skip:
+            sys.stdout.write(f"Transfer syntax UID {ts} is uncompressed, skipping\n")
         else:
-            sys.stderr.write(f"Transfer syntax UID missing\n")
+            sys.stdout.write(f"Transfer syntax UID {ts} not supported, skipping\n")
+
+        if pi in supported_photometric_interpretations:
+            transcode(dataset, pi)
+            sys.stdout.write(f"Transcoding successful from photometric interpretation {pi}\n")
 
         pydicom.dcmwrite(out_file, dataset, write_like_original=False)
 
